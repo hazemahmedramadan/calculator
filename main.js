f98c44b7599e4561b5e0dacb0cede5e28b7d2a11
Hazem Ahmed <126938571+hazemahmedramadan@users.noreply.github.com>
2026-07-01 22:11:03 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const calc_screen = document.querySelector(".screen");
 const calc_numbers = document.querySelectorAll(".numbers button");
 const calc_oprators = document.querySelectorAll(".oprators button");
-let value;
-let result;
 
 calc_numbers.forEach(function (number) {
   number.onclick = function () {
@@ -10,13 +8,12 @@ calc_numbers.forEach(function (number) {
       calc_screen.innerHTML == "0"
       || calc_screen.innerHTML == "Infinity"
       || calc_screen.innerHTML == "NaN"
+      || calc_screen.innerHTML == "Error"
     ) {
       calc_screen.innerHTML = `${number.innerHTML}`;
     } else {
       calc_screen.innerHTML = `${calc_screen.innerHTML}${number.innerHTML}`;
     }
-
-    value = calc_screen.innerHTML;
   };
 });
 
@@ -25,49 +22,39 @@ calc_oprators.forEach(function (oprate) {
     let clicked_oprate = oprate.innerHTML;
     if (clicked_oprate == "c") {
       calc_screen.innerHTML = 0;
-    } else if (clicked_oprate == "+") {
-      calc_screen.innerHTML = `${value}+`;
-    } else if (clicked_oprate == "-") {
-      calc_screen.innerHTML = `${value}-`;
-    } else if (clicked_oprate == "×") {
-      calc_screen.innerHTML = `${value}×`;
-    } else if (clicked_oprate == "/") {
-      calc_screen.innerHTML = `${value}/`;
-    } else if (oprate.classList.contains("back")) {
-      calc_screen.innerHTML = calc_screen.innerHTML.slice(0, -1);
+    }
+
+    if (oprate.classList.contains("back")) {
+      calc_screen.innerHTML.length > 1
+      && !["Infinity", "NaN", "Error"].includes(calc_screen.innerHTML)
+        ? (calc_screen.innerHTML = calc_screen.innerHTML.slice(0, -1))
+        : (calc_screen.innerHTML = 0);
+    }
+
+    if (["+", "/", "-", "×"].includes(clicked_oprate)) {
+      let last_carracter = calc_screen.innerHTML.slice(-1);
+      if (["+", "/", "-", "×"].includes(last_carracter)) {
+        calc_screen.innerHTML =
+          calc_screen.innerHTML.slice(0, -1) + oprate.innerHTML;
+      } else {
+        calc_screen.innerHTML = calc_screen.innerHTML + oprate.innerHTML;
+      }
     }
 
     if (clicked_oprate == "=") {
-      if (calc_screen.innerHTML.includes("×")) {
-        let plus = calc_screen.innerHTML.split("×");
-        result = plus.reduce(function (num, next) {
-          return parseFloat(num) * parseFloat(next);
-        });
-        calc_screen.innerHTML = result;
-      }
-      if (calc_screen.innerHTML.includes("+")) {
-        let plus = calc_screen.innerHTML.split("+");
-        result = plus.reduce(function (num, next) {
-          return parseFloat(num) + parseFloat(next);
-        });
-        calc_screen.innerHTML = result;
-      }
-      if (calc_screen.innerHTML.includes("-")) {
-        let plus = calc_screen.innerHTML.split("-");
-        result = plus.reduce(function (num, next) {
-          return parseFloat(num) - parseFloat(next);
-        });
-        calc_screen.innerHTML = result;
-      }
-      if (calc_screen.innerHTML.includes("/")) {
-        let plus = calc_screen.innerHTML.split("/");
-        result = plus.reduce(function (num, next) {
-          return parseFloat(num) / parseFloat(next);
-        });
-        calc_screen.innerHTML = result;
+      try {
+        let formating = calc_screen.innerHTML.replace(/×/g, "*");
+        let the_resulte = new Function(`return ${formating}`)();
+
+        the_resulte != "Infinity"
+        && the_resulte != "NaN"
+        && the_resulte != undefined
+          ? (calc_screen.innerHTML = `${the_resulte}`)
+          : (calc_screen.innerHTML = "Error");
+      } catch (error) {
+        calc_screen.innerHTML = "Error";
       }
     }
-    value = calc_screen.innerHTML;
   };
 });
 // console.log(calc_oprators)
